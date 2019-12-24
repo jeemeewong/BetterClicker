1afafc4b0ae7db02226330aed697a3b80455e132
jim <jeemeewang@gmail.com>
2019-12-24 14:35:42 +0800

    Initial Commit

---
+++ BetterClicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BetterClicker
+//
+//  Created by Jim Wong on 24/12/19.
+//  Copyright © 2019 Jim Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
